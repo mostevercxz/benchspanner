1ee190990b077f94ef0254ca74b809521e273db5
giant <1234@qq.com>
2025-06-18 23:24:52 +0900

    补提 batch script

--- run_test.bat
+++ run_test.bat
@@ -1,9 +1,17 @@
 go build -o benchmark.exe main.go
 REM set VUS environment variable
-set VUS=10  
+set VUS=1
 set ZONES=10
 set RECORDS=80
 set STRATTRCNT=10
 set INTATTRCNT=10
-benchmark.exe -test=write-vertex
+set ZONE_START=2
+set ZONES_TOTAL=1
+set RECORDS_PER_ZONE=12
+set EDGES_PER_RELATION=1
+@REM benchmark.exe -test=write-vertex
+@REM benchmark.exe -test=write-edge -batch-num 1
+@REM benchmark.exe -test=truncate
+@REM benchmark.exe -test=relation
+benchmark.exe -test=setup
 pause
